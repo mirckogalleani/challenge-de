73efc70cf77feb07f41caaf4093226c6bc2890a3
Mircko Galleani <mgalleani@adelco.cl>
2025-04-03 01:17:04 -0300

    added q3_time function to src

--- src/q3_memory.py
+++ src/q3_memory.py
@@ -1,4 +1,9 @@
 from typing import List, Tuple
+import json
+from collections import defaultdict, Counter
 
+from memory_profiler import profile
+
+@profile
 def q3_memory(file_path: str) -> List[Tuple[str, int]]:
     pass
